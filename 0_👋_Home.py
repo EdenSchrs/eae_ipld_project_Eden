3d1f0cd41cbc50f11241e46f298eda03bfb833b7
Eden Schreurs <edenschreurs@Edens-MacBook-Air.local>
2023-12-19 11:03:21 +0100

    Completed the home page

--- 0_👋_Home.py
+++ 0_👋_Home.py
@@ -24,7 +24,7 @@ st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">�
 
 
 # ----- Profile image file -----
-profile_image_file_path = "profile_image.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+profile_image_file_path = "profile_image.jpg"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
 
 with open(profile_image_file_path, "rb") as img_file:
     img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
@@ -51,7 +51,7 @@ st.subheader("About Me")
 
 # TODO: Modify and adapt the following lines to your info, you can add or remove some details if you want
 st.write("""
-- 🧑‍💻 I am a <Big Data Master Student in Barcelona> 
+- 🧑‍💻 I am a Big Data Master Student in Barcelona
 
 - 🛩️ prev: <Previous Experience, Background or Studies>
 
