d064425c8a452a40fd4241f86035a91c255fd349
EdenSchrs <eden.schreurs@gmail.com>
2023-12-19 19:11:13 +0100

    new changes

--- pages/1_🖼️_Image_Cropper.py
+++ pages/1_🖼️_Image_Cropper.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 # ----- Page configs -----
 st.set_page_config(
-    page_title="<Your Name> Portfolio",
+    page_title="Eden Portfolio",
     page_icon="📊",
 )
 
@@ -42,13 +42,15 @@ st.image(img_arr, caption="Original Image" if not is_example else "Original exam
 st.write("#")
 
 
-# TODO: Ex. 1.1: Get the minimum and maximum values for the vertical and horizontal ranges, so the size of the img_arr array -----
+# Ex. 1.1: Get the minimum and maximum values for the vertical and horizontal ranges, so the size of the img_arr array -----
+
+shape = img_arr.shape
 
 min_height = 0 
-max_height = None   # TODO: Replace None with the maximum height of the image using np.shape() function
+max_height = np.shape[0]   
 
 min_width = 0
-max_width = None    # TODO: Replace None with the maximum width of the image using np.shape() function   
+max_width = np.shape[1]       
 
 
 # ----- Creating the sliders to receive the user input with the dimensions to crop the image ----- 
